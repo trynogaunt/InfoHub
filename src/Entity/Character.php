5b26c8c8a222875b63cd5929a32085c73dbc1edb
Trynogaunt <trynogaunt@gmail.com>
2023-08-19 01:30:37 +0200

    Adding character entity and basics infos

---
+++ src/Entity/Character.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $health = null;
+
+    #[ORM\Column]
+    private ?int $stamina = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $class = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHealth(): ?int
+    {
+        return $this->health;
+    }
+
+    public function setHealth(int $health): static
+    {
+        $this->health = $health;
+
+        return $this;
+    }
+
+    public function getStamina(): ?int
+    {
+        return $this->stamina;
+    }
+
+    public function setStamina(int $stamina): static
+    {
+        $this->stamina = $stamina;
+
+        return $this;
+    }
+
+    public function getClass(): ?string
+    {
+        return $this->class;
+    }
+
+    public function setClass(string $class): static
+    {
+        $this->class = $class;
+
+        return $this;
+    }
+}
